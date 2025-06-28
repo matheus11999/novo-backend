b473996fd813520ad7e581256d3625369df6e597
unknown <matheus119martins@gmail.com>
2025-06-27 21:26:55 -0400

    atualizacao cors

--- src/server.js
+++ src/server.js
@@ -26,6 +26,8 @@ app.use(cors({
         'http://127.0.0.1:3000', 
         'http://localhost:5173', 
         'http://127.0.0.1:5173',
+        'http://localhost:3001',
+        'http://127.0.0.1:3001',
         'https://mikropix.online', 
         'https://api.mikropix.online'
     ],
