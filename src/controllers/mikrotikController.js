00e3cd0417f2649811cbf84b0eda567a4ae047c1
unknown <matheus119martins@gmail.com>
2025-07-10 18:34:43 -0400

    teste

--- src/controllers/mikrotikController.js
+++ src/controllers/mikrotikController.js
@@ -2224,184 +2224,16 @@ const generateInstallRsc = async (req, res) => {
 /ip/firewall/mangle add chain="prerouting" connection-mark="wireguard-conn" action="mark-packet" new-packet-mark="wireguard-packet" comment="Mikropix - Setup"
 
 # Script de limpeza automática - Parte 1: Configuração
-/system/script add name="mikropix-cleanup-vars" source=":global logPrefix \\"AutoRemover-v7\\";
-:global totalUsers 0;
-:global totalBindings 0;
-:global removidosUsers 0;
-:global removidosBindings 0;
-:global ativosUsers 0;
-:global ativosBindings 0;
-:global meses {\\"jan\\"=1;\\"feb\\"=2;\\"mar\\"=3;\\"apr\\"=4;\\"may\\"=5;\\"jun\\"=6;\\"jul\\"=7;\\"aug\\"=8;\\"sep\\"=9;\\"oct\\"=10;\\"nov\\"=11;\\"dec\\"=12};" comment="Mikropix - Setup"
+/system/script add name="mikropix-cleanup-vars" source=":global logPrefix \\"AutoRemover-v7\\"; :global totalUsers 0; :global totalBindings 0; :global removidosUsers 0; :global removidosBindings 0; :global ativosUsers 0; :global ativosBindings 0; :global meses {\\"jan\\"=1;\\"feb\\"=2;\\"mar\\"=3;\\"apr\\"=4;\\"may\\"=5;\\"jun\\"=6;\\"jul\\"=7;\\"aug\\"=8;\\"sep\\"=9;\\"oct\\"=10;\\"nov\\"=11;\\"dec\\"=12};" comment="Mikropix - Setup"
 
 # Script de limpeza automática - Parte 2: Processamento de usuários
-/system/script add name="mikropix-cleanup-users" source=":global logPrefix;
-:global totalUsers;
-:global removidosUsers;
-:global ativosUsers;
-:global meses;
-:local tempoAtual [/system clock get time];
-:local dataAtual [/system clock get date];
-:local mesAtualNum;
-:local diaAtual;
-:local anoAtual;
-:if ([:pick \\$dataAtual 4 5] = \\"/\\") do={
-    :set mesAtualNum [:tonum [:pick \\$dataAtual 5 7]];
-    :set diaAtual [:tonum [:pick \\$dataAtual 8 10]];
-    :set anoAtual [:tonum [:pick \\$dataAtual 0 4]];
-} else={
-    :if ([:pick \\$dataAtual 4 5] = \\"-\\") do={
-        :set mesAtualNum [:tonum [:pick \\$dataAtual 5 7]];
-        :set diaAtual [:tonum [:pick \\$dataAtual 8 10]];
-        :set anoAtual [:tonum [:pick \\$dataAtual 0 4]];
-    } else={
-        :set mesAtualNum (\\$meses->[:tolower [:pick \\$dataAtual 4 7]]);
-        :set diaAtual [:tonum [:pick \\$dataAtual 8 10]];
-        :set anoAtual [:tonum [:pick \\$dataAtual 0 4]];
-    };
-};
-:local horaAtualNum [:tonum [:pick \\$tempoAtual 0 2]];
-:local minAtualNum [:tonum [:pick \\$tempoAtual 3 5]];
-:foreach i in=[/ip hotspot user find where comment~\\"Expira:\\"] do={
-    :set totalUsers (\\$totalUsers + 1);
-    :local userName [/ip hotspot user get \\$i name];
-    :local userComment [/ip hotspot user get \\$i comment];
-    :local posInicio ([:find \\$userComment \\"Expira: \\"] + 8);
-    :if (\\$posInicio > 7) do={
-        :local dataExpCompleta [:pick \\$userComment \\$posInicio [:len \\$userComment]];
-        :if ([:len \\$dataExpCompleta] >= 16) do={
-            :local dataExp [:pick \\$dataExpCompleta 0 10];
-            :local horaExp [:pick \\$dataExpCompleta 11 16];
-            :local diaExp [:tonum [:pick \\$dataExp 0 2]];
-            :local mesExp [:tonum [:pick \\$dataExp 3 5]];
-            :local anoExp [:tonum [:pick \\$dataExp 6 10]];
-            :local horaExpNum [:tonum [:pick \\$horaExp 0 2]];
-            :local minExpNum [:tonum [:pick \\$horaExp 3 5]];
-            :if ([:typeof \\$diaExp] = \\"num\\" && [:typeof \\$mesExp] = \\"num\\" && [:typeof \\$anoExp] = \\"num\\" && [:typeof \\$horaExpNum] = \\"num\\" && [:typeof \\$minExpNum] = \\"num\\") do={
-                :local expirado false;
-                :if (\\$anoExp < \\$anoAtual) do={ :set expirado true };
-                :if (\\$anoExp = \\$anoAtual && \\$mesExp < \\$mesAtualNum) do={ :set expirado true };
-                :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp < \\$diaAtual) do={ :set expirado true };
-                :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp = \\$diaAtual && \\$horaExpNum < \\$horaAtualNum) do={ :set expirado true };
-                :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp = \\$diaAtual && \\$horaExpNum = \\$horaAtualNum && \\$minExpNum <= \\$minAtualNum) do={ :set expirado true };
-                :if (\\$expirado) do={
-                    :log warning \\"[\\$logPrefix] Hotspot User: '\\$userName' expirou. Removendo.\\";
-                    /ip hotspot user remove \\$i;
-                    :set removidosUsers (\\$removidosUsers + 1);
-                } else={
-                    :set ativosUsers (\\$ativosUsers + 1);
-                };
-            };
-        };
-    };
-};" comment="Mikropix - Setup"
+/system/script add name="mikropix-cleanup-users" source=":global logPrefix; :global totalUsers; :global removidosUsers; :global ativosUsers; :global meses; :local tempoAtual [/system clock get time]; :local dataAtual [/system clock get date]; :local mesAtualNum; :local diaAtual; :local anoAtual; :if ([:pick \\$dataAtual 4 5] = \\"/\\") do={ :set mesAtualNum [:tonum [:pick \\$dataAtual 5 7]]; :set diaAtual [:tonum [:pick \\$dataAtual 8 10]]; :set anoAtual [:tonum [:pick \\$dataAtual 0 4]]; } else={ :if ([:pick \\$dataAtual 4 5] = \\"-\\") do={ :set mesAtualNum [:tonum [:pick \\$dataAtual 5 7]]; :set diaAtual [:tonum [:pick \\$dataAtual 8 10]]; :set anoAtual [:tonum [:pick \\$dataAtual 0 4]]; } else={ :set mesAtualNum (\\$meses->[:tolower [:pick \\$dataAtual 4 7]]); :set diaAtual [:tonum [:pick \\$dataAtual 8 10]]; :set anoAtual [:tonum [:pick \\$dataAtual 0 4]]; }; }; :local horaAtualNum [:tonum [:pick \\$tempoAtual 0 2]]; :local minAtualNum [:tonum [:pick \\$tempoAtual 3 5]]; :foreach i in=[/ip hotspot user find where comment~\\"Expira:\\"] do={ :set totalUsers (\\$totalUsers + 1); :local userName [/ip hotspot user get \\$i name]; :local userComment [/ip hotspot user get \\$i comment]; :local posInicio ([:find \\$userComment \\"Expira: \\"] + 8); :if (\\$posInicio > 7) do={ :local dataExpCompleta [:pick \\$userComment \\$posInicio [:len \\$userComment]]; :if ([:len \\$dataExpCompleta] >= 16) do={ :local dataExp [:pick \\$dataExpCompleta 0 10]; :local horaExp [:pick \\$dataExpCompleta 11 16]; :local diaExp [:tonum [:pick \\$dataExp 0 2]]; :local mesExp [:tonum [:pick \\$dataExp 3 5]]; :local anoExp [:tonum [:pick \\$dataExp 6 10]]; :local horaExpNum [:tonum [:pick \\$horaExp 0 2]]; :local minExpNum [:tonum [:pick \\$horaExp 3 5]]; :if ([:typeof \\$diaExp] = \\"num\\" && [:typeof \\$mesExp] = \\"num\\" && [:typeof \\$anoExp] = \\"num\\" && [:typeof \\$horaExpNum] = \\"num\\" && [:typeof \\$minExpNum] = \\"num\\") do={ :local expirado false; :if (\\$anoExp < \\$anoAtual) do={ :set expirado true }; :if (\\$anoExp = \\$anoAtual && \\$mesExp < \\$mesAtualNum) do={ :set expirado true }; :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp < \\$diaAtual) do={ :set expirado true }; :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp = \\$diaAtual && \\$horaExpNum < \\$horaAtualNum) do={ :set expirado true }; :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp = \\$diaAtual && \\$horaExpNum = \\$horaAtualNum && \\$minExpNum <= \\$minAtualNum) do={ :set expirado true }; :if (\\$expirado) do={ :log warning \\"[\\$logPrefix] Hotspot User: '\\$userName' expirou. Removendo.\\"; /ip hotspot user remove \\$i; :set removidosUsers (\\$removidosUsers + 1); } else={ :set ativosUsers (\\$ativosUsers + 1); }; }; }; };" comment="Mikropix - Setup"
 
 # Script de limpeza automática - Parte 3: Processamento de IP bindings
-/system/script add name="mikropix-cleanup-bindings" source=":global logPrefix;
-:global totalBindings;
-:global removidosBindings;
-:global ativosBindings;
-:global meses;
-:local tempoAtual [/system clock get time];
-:local dataAtual [/system clock get date];
-:local mesAtualNum;
-:local diaAtual;
-:local anoAtual;
-:if ([:pick \\$dataAtual 4 5] = \\"/\\") do={
-    :set mesAtualNum [:tonum [:pick \\$dataAtual 5 7]];
-    :set diaAtual [:tonum [:pick \\$dataAtual 8 10]];
-    :set anoAtual [:tonum [:pick \\$dataAtual 0 4]];
-} else={
-    :if ([:pick \\$dataAtual 4 5] = \\"-\\") do={
-        :set mesAtualNum [:tonum [:pick \\$dataAtual 5 7]];
-        :set diaAtual [:tonum [:pick \\$dataAtual 8 10]];
-        :set anoAtual [:tonum [:pick \\$dataAtual 0 4]];
-    } else={
-        :set mesAtualNum (\\$meses->[:tolower [:pick \\$dataAtual 4 7]]);
-        :set diaAtual [:tonum [:pick \\$dataAtual 8 10]];
-        :set anoAtual [:tonum [:pick \\$dataAtual 0 4]];
-    };
-};
-:local horaAtualNum [:tonum [:pick \\$tempoAtual 0 2]];
-:local minAtualNum [:tonum [:pick \\$tempoAtual 3 5]];
-:foreach i in=[/ip hotspot ip-binding find where comment~\\"Expira:\\"] do={
-    :set totalBindings (\\$totalBindings + 1);
-    :local bindingMac [/ip hotspot ip-binding get \\$i mac-address];
-    :local bindingComment [/ip hotspot ip-binding get \\$i comment];
-    :if ([:len \\$bindingMac] > 0) do={
-        :local posInicio ([:find \\$bindingComment \\"Expira: \\"] + 8);
-        :if (\\$posInicio > 7) do={
-            :local dataExpCompleta [:pick \\$bindingComment \\$posInicio [:len \\$bindingComment]];
-            :local dataExp \\"\\";
-            :local horaExp \\"\\";
-            :if ([:find \\$dataExpCompleta \\"-\\"] != -1) do={
-                :local ano [:pick \\$dataExpCompleta 0 4];
-                :local mes [:pick \\$dataExpCompleta 5 7];
-                :local dia [:pick \\$dataExpCompleta 8 10];
-                :local hora [:pick \\$dataExpCompleta 11 16];
-                :set dataExp \\"\\$dia/\\$mes/\\$ano\\";
-                :set horaExp \\$hora;
-            } else={
-                :if ([:len \\$dataExpCompleta] >= 16) do={
-                    :set dataExp [:pick \\$dataExpCompleta 0 10];
-                    :set horaExp [:pick \\$dataExpCompleta 11 16];
-                };
-            };
-            :if ([:len \\$dataExp] = 10 && [:len \\$horaExp] = 5) do={
-                :local diaExp [:tonum [:pick \\$dataExp 0 2]];
-                :local mesExp [:tonum [:pick \\$dataExp 3 5]];
-                :local anoExp [:tonum [:pick \\$dataExp 6 10]];
-                :local horaExpNum [:tonum [:pick \\$horaExp 0 2]];
-                :local minExpNum [:tonum [:pick \\$horaExp 3 5]];
-                :if ([:typeof \\$diaExp] = \\"num\\" && [:typeof \\$mesExp] = \\"num\\" && [:typeof \\$anoExp] = \\"num\\" && [:typeof \\$horaExpNum] = \\"num\\" && [:typeof \\$minExpNum] = \\"num\\") do={
-                    :local expirado false;
-                    :if (\\$anoExp < \\$anoAtual) do={ :set expirado true };
-                    :if (\\$anoExp = \\$anoAtual && \\$mesExp < \\$mesAtualNum) do={ :set expirado true };
-                    :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp < \\$diaAtual) do={ :set expirado true };
-                    :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp = \\$diaAtual && \\$horaExpNum < \\$horaAtualNum) do={ :set expirado true };
-                    :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp = \\$diaAtual && \\$horaExpNum = \\$horaAtualNum && \\$minExpNum <= \\$minAtualNum) do={ :set expirado true };
-                    :if (\\$expirado) do={
-                        :log warning \\"[\\$logPrefix] IP Binding MAC: '\\$bindingMac' expirou. Removendo.\\";
-                        /ip hotspot ip-binding remove \\$i;
-                        :set removidosBindings (\\$removidosBindings + 1);
-                    } else={
-                        :set ativosBindings (\\$ativosBindings + 1);
-                    };
-                };
-            };
-        };
-    };
-};" comment="Mikropix - Setup"
+/system/script add name="mikropix-cleanup-bindings" source=":global logPrefix; :global totalBindings; :global removidosBindings; :global ativosBindings; :global meses; :local tempoAtual [/system clock get time]; :local dataAtual [/system clock get date]; :local mesAtualNum; :local diaAtual; :local anoAtual; :if ([:pick \\$dataAtual 4 5] = \\"/\\") do={ :set mesAtualNum [:tonum [:pick \\$dataAtual 5 7]]; :set diaAtual [:tonum [:pick \\$dataAtual 8 10]]; :set anoAtual [:tonum [:pick \\$dataAtual 0 4]]; } else={ :if ([:pick \\$dataAtual 4 5] = \\"-\\") do={ :set mesAtualNum [:tonum [:pick \\$dataAtual 5 7]]; :set diaAtual [:tonum [:pick \\$dataAtual 8 10]]; :set anoAtual [:tonum [:pick \\$dataAtual 0 4]]; } else={ :set mesAtualNum (\\$meses->[:tolower [:pick \\$dataAtual 4 7]]); :set diaAtual [:tonum [:pick \\$dataAtual 8 10]]; :set anoAtual [:tonum [:pick \\$dataAtual 0 4]]; }; }; :local horaAtualNum [:tonum [:pick \\$tempoAtual 0 2]]; :local minAtualNum [:tonum [:pick \\$tempoAtual 3 5]]; :foreach i in=[/ip hotspot ip-binding find where comment~\\"Expira:\\"] do={ :set totalBindings (\\$totalBindings + 1); :local bindingMac [/ip hotspot ip-binding get \\$i mac-address]; :local bindingComment [/ip hotspot ip-binding get \\$i comment]; :if ([:len \\$bindingMac] > 0) do={ :local posInicio ([:find \\$bindingComment \\"Expira: \\"] + 8); :if (\\$posInicio > 7) do={ :local dataExpCompleta [:pick \\$bindingComment \\$posInicio [:len \\$bindingComment]]; :local dataExp \\"\\"; :local horaExp \\"\\"; :if ([:find \\$dataExpCompleta \\"-\\"] != -1) do={ :local ano [:pick \\$dataExpCompleta 0 4]; :local mes [:pick \\$dataExpCompleta 5 7]; :local dia [:pick \\$dataExpCompleta 8 10]; :local hora [:pick \\$dataExpCompleta 11 16]; :set dataExp \\"\\$dia/\\$mes/\\$ano\\"; :set horaExp \\$hora; } else={ :if ([:len \\$dataExpCompleta] >= 16) do={ :set dataExp [:pick \\$dataExpCompleta 0 10]; :set horaExp [:pick \\$dataExpCompleta 11 16]; }; }; :if ([:len \\$dataExp] = 10 && [:len \\$horaExp] = 5) do={ :local diaExp [:tonum [:pick \\$dataExp 0 2]]; :local mesExp [:tonum [:pick \\$dataExp 3 5]]; :local anoExp [:tonum [:pick \\$dataExp 6 10]]; :local horaExpNum [:tonum [:pick \\$horaExp 0 2]]; :local minExpNum [:tonum [:pick \\$horaExp 3 5]]; :if ([:typeof \\$diaExp] = \\"num\\" && [:typeof \\$mesExp] = \\"num\\" && [:typeof \\$anoExp] = \\"num\\" && [:typeof \\$horaExpNum] = \\"num\\" && [:typeof \\$minExpNum] = \\"num\\") do={ :local expirado false; :if (\\$anoExp < \\$anoAtual) do={ :set expirado true }; :if (\\$anoExp = \\$anoAtual && \\$mesExp < \\$mesAtualNum) do={ :set expirado true }; :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp < \\$diaAtual) do={ :set expirado true }; :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp = \\$diaAtual && \\$horaExpNum < \\$horaAtualNum) do={ :set expirado true }; :if (\\$anoExp = \\$anoAtual && \\$mesExp = \\$mesAtualNum && \\$diaExp = \\$diaAtual && \\$horaExpNum = \\$horaAtualNum && \\$minExpNum <= \\$minAtualNum) do={ :set expirado true }; :if (\\$expirado) do={ :log warning \\"[\\$logPrefix] IP Binding MAC: '\\$bindingMac' expirou. Removendo.\\"; /ip hotspot ip-binding remove \\$i; :set removidosBindings (\\$removidosBindings + 1); } else={ :set ativosBindings (\\$ativosBindings + 1); }; }; }; };" comment="Mikropix - Setup"
 
 # Script principal que executa tudo
-/system/script add name="mikropix-cleanup" source=":global logPrefix;
-:global totalUsers;
-:global totalBindings;
-:global removidosUsers;
-:global removidosBindings;
-:global ativosUsers;
-:global ativosBindings;
-:local tempoAtual [/system clock get time];
-:local dataAtual [/system clock get date];
-:log info \\"[\\$logPrefix] Iniciando verificação. Data/Hora atual: \\$dataAtual \\$tempoAtual\\";
-:set totalUsers 0;
-:set totalBindings 0;
-:set removidosUsers 0;
-:set removidosBindings 0;
-:set ativosUsers 0;
-:set ativosBindings 0;
-:do {
-    /system script run mikropix-cleanup-vars;
-    /system script run mikropix-cleanup-users;
-    /system script run mikropix-cleanup-bindings;
-    :log info \\"[\\$logPrefix] ========== RELATÓRIO FINAL ==========\\";
-    :log info \\"[\\$logPrefix] HOTSPOT USERS: Total=\\$totalUsers | Ativos=\\$ativosUsers | Removidos=\\$removidosUsers\\";
-    :log info \\"[\\$logPrefix] IP BINDINGS: Total=\\$totalBindings | Ativos=\\$ativosBindings | Removidos=\\$removidosBindings\\";
-    :log info \\"[\\$logPrefix] TOTAL REMOVIDOS: \\$(\\$removidosUsers + \\$removidosBindings)\\";
-    :log info \\"[\\$logPrefix] Verificação concluída.\\";
-} on-error={
-    :log error \\"[\\$logPrefix] Erro durante a execução do script.\\";
-};" comment="Mikropix - Setup"
-
+/system/script add name="mikropix-cleanup" source=":global logPrefix; :global totalUsers; :global totalBindings; :global removidosUsers; :global removidosBindings; :global ativosUsers; :global ativosBindings; :local tempoAtual [/system clock get time]; :local dataAtual [/system clock get date]; :log info \\"[\\$logPrefix] Iniciando verificação. Data/Hora atual: \\$dataAtual \\$tempoAtual\\"; :set totalUsers 0; :set totalBindings 0; :set removidosUsers 0; :set removidosBindings 0; :set ativosUsers 0; :set ativosBindings 0; :do { /system script run mikropix-cleanup-vars; /system script run mikropix-cleanup-users; /system script run mikropix-cleanup-bindings; :log info \\"[\\$logPrefix] ========== RELATÓRIO FINAL ==========\\"; :log info \\"[\\$logPrefix] HOTSPOT USERS: Total=\\$totalUsers | Ativos=\\$ativosUsers | Removidos=\\$removidosUsers\\"; :log info \\"[\\$logPrefix] IP BINDINGS: Total=\\$totalBindings | Ativos=\\$ativosBindings | Removidos=\\$removidosBindings\\"; :log info \\"[\\$logPrefix] TOTAL REMOVIDOS: \\$(\\$removidosUsers + \\$removidosBindings)\\"; :log info \\"[\\$logPrefix] Verificação concluída.\\"; } on-error={ :log error \\"[\\$logPrefix] Erro durante a execução do script.\\"; };" comment="Mikropix - Setup"
 /system/scheduler add name="mikropix-cleanup-scheduler" interval=2m on-event="mikropix-cleanup" comment="Mikropix - Setup"
 /interface/wireguard set [find name="wg-client"] disabled=no
 :log info "Configuração Mikropix instalada com sucesso"`;
@@ -2433,17 +2265,18 @@ const generateUninstallRsc = async (req, res) => {
     }
 
     // Gerar conteúdo do arquivo RSC de desinstalação
-    const rscContent = `/system/scheduler remove [find comment="Mikropix - Setup"]
-/system/script remove [find comment="Mikropix - Setup"]
+    const rscContent = `/system/scheduler remove [find name="mikropix-cleanup-scheduler"]
+/system/script remove [find name="mikropix-cleanup"]
+/system/script remove [find name="mikropix-cleanup-bindings"]
+/system/script remove [find name="mikropix-cleanup-users"]
+/system/script remove [find name="mikropix-cleanup-vars"]
 /ip/firewall/mangle remove [find comment="Mikropix - Setup"]
 /ip/firewall/nat remove [find comment="Mikropix - Setup"]
 /ip/firewall/filter remove [find comment="Mikropix - Setup"]
-/ip/hotspot/walled-garden remove [find comment="Mikropix - Setup"]
 /ip/address remove [find comment="Mikropix - Setup"]
 /interface/wireguard/peers remove [find comment="Mikropix - Setup"]
-/interface/wireguard remove [find name="wg-client"]
-/system/clock set time-zone-name="auto"
-/system/ntp/client set enabled=no primary-ntp="" secondary-ntp=""
+/interface/wireguard remove [find comment="Mikropix - Setup"]
+/ip/hotspot/walled-garden remove [find comment="Mikropix - Setup"]
 :log info "Configuração Mikropix removida com sucesso"`;
 
     // Retornar o arquivo RSC
